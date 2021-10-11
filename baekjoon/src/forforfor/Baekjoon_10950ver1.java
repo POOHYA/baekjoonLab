95a4873037f162482b4ab76c5e06cece03ce737d
POOHYA <hyoju2259@gmail.com>
2021-10-11 22:15:46 +0900

    이름수정

--- baekjoon/src/forforfor/Baekjoon_10950ver1.java
+++ baekjoon/src/forforfor/Baekjoon_10950ver1.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Beakjoon_10950ver1 {
+public class Baekjoon_10950ver1 {
 
 //	public static void main(String[] args) throws NumberFormatException, IOException {
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
